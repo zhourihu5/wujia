dcc2dd846d9b793816d7a3f9a2034ba9f6f12e70
a-zhourihu <rihu.zhou@dhc.com.cn>
2019-08-28 14:20:28 +0800

    group buy

--- market/src/main/java/com/jingxi/smartlife/pad/market/mvp/adapter/OrderAdapter.kt
+++ market/src/main/java/com/jingxi/smartlife/pad/market/mvp/adapter/OrderAdapter.kt
@@ -10,6 +10,8 @@ import com.jingxi.smartlife.pad.market.mvp.data.OrderItemVo
 import com.wujia.lib.imageloader.ImageLoaderManager
 import com.wujia.lib_common.base.baseadapter.CommonAdapter
 import com.wujia.lib_common.base.baseadapter.base.ViewHolder
+import com.wujia.lib_common.utils.DateUtil
+import java.util.*
 
 /**
  * Author: created by shenbingkai on 2018/12/11 11 24
@@ -20,50 +22,111 @@ class OrderAdapter(context: Context, datas: List<OrderItemVo>) : CommonAdapter<O
 
     var btnClickLisner:((holder: ViewHolder, t: OrderItemVo, position: Int)->Unit)?=null
     override fun convert(holder: ViewHolder, t: OrderItemVo, position: Int) {
-        val img = holder.getView<ImageView>(R.id.ivCover)
-        ImageLoaderManager.instance.loadImage(t.activity.cover, 0, img)
-        holder.setText(R.id.tvName,t.activity.title)
-        val priceArr= t.activity.price.split('.')
-        holder.setText(R.id.tvPriceInt,priceArr[0])
-        if(priceArr.size>1){
-            holder.setText(R.id.tvFloat,".${priceArr[1]}")
-        }
-        holder.setText(R.id.tvPriceOld,"￥${t.commodity.price}")
-        holder.getView<TextView>(R.id.tvPriceOld).paint.flags= Paint.STRIKE_THRU_TEXT_FLAG
-//        holder.setText(R.id.tvSaleNum,"已抢购${t.commodity.salesNum}件")
 
-        holder.setVisible(R.id.tvToPay,false)
-        holder.setVisible(R.id.tvGoStroll,false)
-        holder.setVisible(R.id.tvAskDilivery,false)
+
+
+
+        val ivCover = holder.getView<ImageView>(R.id.ivCover)
+        val tvOrderStatus=holder.getView<TextView>(R.id.tvOrderStatus)
+        val tvToptitle=holder.getView<TextView>(R.id.tvToptitle)
+        val tvTimeToEnd=holder.getView<TextView>(R.id.tvTimeToEnd)
+        val tvPriceOld=holder.getView<TextView>(R.id.tvPriceOld)
+        val tvName=holder.getView<TextView>(R.id.tvName)
+        val tvPriceInt=holder.getView<TextView>(R.id.tvPriceInt)
+        val tvFloat=holder.getView<TextView>(R.id.tvFloat)
+        val tvToPay=holder.getView<TextView>(R.id.tvToPay)
+        val tvGoStroll=holder.getView<TextView>(R.id.tvGoStroll)
+        val tvAskDilivery=holder.getView<TextView>(R.id.tvAskDilivery)
+
+        tvToPay.visibility=View.GONE
+        tvGoStroll.visibility=View.GONE
+        tvAskDilivery.visibility=View.GONE
+
+        tvToPay.setOnClickListener {
+            btnClickLisner?.invoke(holder,t,position)
+        }
+        tvGoStroll.setOnClickListener {
+            btnClickLisner?.invoke(holder,t,position)
+        }
+        tvAskDilivery.setOnClickListener {
+            btnClickLisner?.invoke(holder,t,position)
+        }
+
+        ImageLoaderManager.instance.loadImage(t.activity.cover, 0, ivCover)
+        tvPriceOld.setText("￥${t.commodity.price}")
+        tvPriceOld.paint.flags= Paint.STRIKE_THRU_TEXT_FLAG
+
+        tvName.setText(t.activity.title)
+        val priceArr= t.activity.price.split('.')
+        tvPriceInt.setText(priceArr[0])
+        if(priceArr.size>1){
+            tvFloat.setText(".${priceArr[1]}")
+        }
+
+        tvTimeToEnd.setText("")
+        tvOrderStatus.setTextColor(0xBA9C77)
+        tvPriceInt.setTextColor(0xC8AC88)
+        tvFloat.setTextColor(0xC8AC88)
+        tvName.setTextColor(0x333333)
+
+
         when(t.status){
             "1"->{//待付款
-                holder.setVisible(R.id.tvToPay,true)
+                tvToPay.visibility=View.VISIBLE
+                val endDate = DateUtil.getDate(t.payEndDate)
+                setTimeRemain(endDate,tvTimeToEnd)
+                tvToptitle.setText("付款剩余时间：")
+                tvOrderStatus.setText("待付款")
             }
             "2"->{//待配送
+                tvToptitle.setText("配送小哥正在取货中…")
+                tvOrderStatus.setText("待收货")
             }
             "3"->{//已收货
-                holder.setVisible(R.id.tvGoStroll,true)
+                tvGoStroll.visibility=View.VISIBLE
+                tvToptitle.setText("收货时间：${t.receiveDate}")
+                tvOrderStatus.setText("已收货")
+
             }
             "4"->{//已过期
-                holder.setVisible(R.id.tvGoStroll,true)
+                tvGoStroll.visibility=View.VISIBLE
+                tvToptitle.setText("过期时间：${t.payEndDate}")
+                tvOrderStatus.setText("已过期")
+                tvOrderStatus.setTextColor(0x888888)
+                tvPriceInt.setTextColor(0x888888)
+                tvFloat.setTextColor(0x888888)
+                tvName.setTextColor(0x888888)
             }
             "5"->{//配送中
-                holder.setVisible(R.id.tvAskDilivery,true)
+                tvAskDilivery.visibility=View.VISIBLE
+                tvToptitle.setText("送货人：${t.deliveryPerson}")
+                tvOrderStatus.setText("待收货")
             }
 
         }
-        holder.getView<View>(R.id.tvToPay).setOnClickListener {
-            btnClickLisner?.invoke(holder,t,position)
+
+    }
+    fun setTimeRemain(endDate:Date,tvTimeToEnd:TextView) {
+        var now = Date();
+        var milli = endDate!!.getTime() - now.getTime()
+        if (milli <= 0) {
+            tvTimeToEnd.setText("00： 00：00 ")
+            return
         }
-        holder.getView<View>(R.id.tvAskDilivery).setOnClickListener {
-            btnClickLisner?.invoke(holder,t,position)
-        }
-        holder.getView<View>(R.id.tvGoStroll).setOnClickListener {
-            btnClickLisner?.invoke(holder,t,position)
-        }
+        var hour = milli / 1000 / 3600
+        var minute = milli % (3600 * 1000) / (60 * 1000)
+        var second = milli % (1000 * 60) / 1000
+
+        tvTimeToEnd.setText("待付款时间：${formatNumber(hour)}：${formatNumber(minute)}：${formatNumber(second)}")
 
 
     }
-
+    fun formatNumber(n: Long): String {
+        var t = ""
+        if (n < 10) {
+            t = "0"
+        }
+        return "${t}${n}"
+    }
 
 }
